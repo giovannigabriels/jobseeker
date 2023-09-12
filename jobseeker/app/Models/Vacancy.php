05ef09513eb5f9e18274a3570e466cc4f9e5aa3d
giovannigabriels <giovanni@eigen.co.id>
2023-09-12 17:07:53 +0700

    set rules update for candidate and vacancy, feature delete and update

--- jobseeker/app/Models/Vacancy.php
+++ jobseeker/app/Models/Vacancy.php
@@ -4,19 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Vacancy extends Model
 {
     protected $table = 'vacancies';
+    protected $primaryKey = 'vacancy_id';
 
     protected $fillable = [
-        'vacancy_id',
         'vacancy_name',
         'min_age',
         'max_age',
         'requirement_gender',
         'expired_date',
     ];
+
+  
+
+    protected $rules = [
+        'vacancy_name' => 'unique:vacancies',
+    ];
+
+    public function validate(array $data)
+    {
+        return Validator::make($data, $this->rules);
+    }
 
     public function candidateApplies()
     {
